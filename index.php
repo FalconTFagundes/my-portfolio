c46865c110e9870086bf2d05aa4f933d6e1637ea
FalconTFagundes <rafaelfagundes762@gmail.com>
2025-03-15 18:44:23 -0300

    adicionando header + nav com complementos

--- index.php
+++ index.php
@@ -1,13 +1,61 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fagundes</title>
-    <link rel="stylesheet" href="./css/style.css">
-    <link rel="shortcut icon" href="./favicon/Rafael-Fagundes-Icone.ico" type="image/x-icon">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Fagundes</title>
+
+  <!-- link font Montserrat -->
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap" rel="stylesheet">
+
+  <!-- boxicons -->
+  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+  <!-- link favicon rafael.f -->
+  <link rel="shortcut icon" href="./favicon/Rafael-Fagundes-Icone.ico" type="image/x-icon">
+
+  <!-- Link Booststrap css-->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
-    
+  <header class="container-fluid" style="background-color: black;">
+    <div class="container text-center" style="background-color: black;">
+      <nav class="d-flex align-items-center justify-content-between p-3">
+
+        <a class="navbar-brand" href="#">
+          <img src="img/falconImg.png" alt="FalconTFagundes" height="120" width="120">
+        </a>
+
+        <ul class="list-unstyled d-flex text-light align-items-center m-0">
+          <li class="ms-5"><a class="text-decoration-none navTitles" href="#sobre">SOBRE</a></li>
+          <li class="ms-5"><a class="text-decoration-none navTitles" href="#projetos">PROJETOS</a></li>
+          <li class="ms-5"><a class="text-decoration-none navTitles" href="#serviços">SERVIÇOS</a></li>
+          <li class="ms-5"><a class="text-decoration-none navTitles" href="#contato">CONTATO</a></li>
+          <li class="ms-5"><a class="text-decoration-none navTitles" href="#linkedin"><i class='bx bxl-linkedin fs-1'></i></a></li>
+          <li class="ms-5"><a class="text-decoration-none navTitles" href="#instagram"><i class='bx bxl-instagram fs-1'></i></a></li>
+          <li class="ms-5"><a class="text-decoration-none navTitles" href="#github"><i class='bx bxl-github fs-1'></i></a></li>
+          <li class="ms-5"><a class="text-decoration-none navTitles" href="#facebook"><i class='bx bxl-facebook fs-1'></i></a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+
+
+
+
+  <!-- Linkk bootstrap js + popper -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+    integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+    integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
+    crossorigin="anonymous"></script>
+
+
 </body>
+
 </html>
