552afa28ea1b6824a56b323c5e33e01bdc522d03
FalconTFagundes <rafaelfagundes762@gmail.com>
2025-03-16 20:34:34 -0300

    adicionando ícone de alterar modo claro/escuro e adicionando alternancia do mesmo com js

--- js/func.js
+++ js/func.js
@@ -1,4 +1,11 @@
 document.getElementById("toggle-dark-mode").addEventListener("click", function() {
     document.body.classList.toggle("dark-mode");
+    
+    const icon = document.getElementById("toggle-dark-mode");
+    if (document.body.classList.contains("dark-mode")) {
+      icon.classList.replace("bxs-brightness-half", "bx-brightness-half");
+    } else {
+      icon.classList.replace("bx-brightness-half", "bxs-brightness-half");
+    }
   });
   
