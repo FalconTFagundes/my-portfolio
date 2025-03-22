6579ae727ad688abe01cd73c4fdd076b9b17960b
Rafael <rafaelfagundes762@gmail.com>
2025-03-22 03:28:28 -0300

    adicionando logica de coletar o que estiver armazenado no localstorage para continuar mantendo modo escuro ou claro

--- assets/js/func.js
+++ assets/js/func.js
@@ -1,13 +1,36 @@
 document.getElementById("toggle-dark-mode").addEventListener("click", function () {
+
   document.body.classList.toggle("dark-mode");
+
 
   const icon = document.getElementById("toggle-dark-mode");
   if (document.body.classList.contains("dark-mode")) {
     icon.classList.replace("bxs-brightness-half", "bx-brightness-half");
+    // salvando a preferência no localStorage
+    localStorage.setItem("theme", "dark");
   } else {
+    icon.classList.replace("bx-brightness-half", "bxs-brightness-half");
+
+    localStorage.setItem("theme", "light");
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const savedTheme = localStorage.getItem("theme");
+
+  // se for "dark", aplica o modo escuro
+  if (savedTheme === "dark") {
+    document.body.classList.add("dark-mode");
+    const icon = document.getElementById("toggle-dark-mode");
+    icon.classList.replace("bxs-brightness-half", "bx-brightness-half");
+  } else {
+    // se não for "dark", não aplica
+    document.body.classList.remove("dark-mode");
+    const icon = document.getElementById("toggle-dark-mode");
     icon.classList.replace("bx-brightness-half", "bxs-brightness-half");
   }
 });
+
 
 function erroDisabledButton() {
   Swal.fire({
