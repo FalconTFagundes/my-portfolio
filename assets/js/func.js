ee01fa8752a680da830f090f414be177f128f61d
Rafael <rafaelfagundes762@gmail.com>
2025-03-24 15:48:01 -0300

    adicionado form de contato whatsapp

--- assets/js/func.js
+++ assets/js/func.js
@@ -1,38 +1,17 @@
-/* Modo escuro - Desabilitado */
+/* script mensagem whatsapp */
 
-/* document.getElementById("toggle-dark-mode").addEventListener("click", function () {
-
-  document.body.classList.toggle("dark-mode");
-
-
-  const icon = document.getElementById("toggle-dark-mode");
-  if (document.body.classList.contains("dark-mode")) {
-    icon.classList.replace("bxs-brightness-half", "bx-brightness-half");
-    // salvando a preferência no localStorage
-    localStorage.setItem("theme", "dark");
-  } else {
-    icon.classList.replace("bx-brightness-half", "bxs-brightness-half");
-
-    localStorage.setItem("theme", "light");
+function sendToWhatsApp() {
+  let message = document.getElementById("message").value.trim();
+  let phoneNumber = "5533991414767"; // Substitua pelo seu número (código do país + número)
+  
+  if (message === "") {
+      alert("Digite uma mensagem antes de enviar!");
+      return;
   }
-});
 
-document.addEventListener("DOMContentLoaded", function () {
-  const savedTheme = localStorage.getItem("theme");
-
-  // se for "dark", aplica o modo escuro
-  if (savedTheme === "dark") {
-    document.body.classList.add("dark-mode");
-    const icon = document.getElementById("toggle-dark-mode");
-    icon.classList.replace("bxs-brightness-half", "bx-brightness-half");
-  } else {
-    // se não for "dark", não aplica
-    document.body.classList.remove("dark-mode");
-    const icon = document.getElementById("toggle-dark-mode");
-    icon.classList.replace("bx-brightness-half", "bxs-brightness-half");
-  }
-});
- */
+  let url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+  window.open(url, "_blank");
+}
 
 function erroDisabledButton() {
   Swal.fire({
